8067a92f874bbf92c3f68313caa642cbe548e9cc
Harsh Chaurasiya <harshchaurasiya6768@gmail.com>
2023-04-05 16:54:51 +0530

    lombok : lombok java library added to reduce boilerplate codes for getter, setter , constructor

--- src/main/java/com/technohack/utils/CustomResponse.java
+++ src/main/java/com/technohack/utils/CustomResponse.java
@@ -1,41 +1,18 @@
 package com.technohack.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomResponse {
     private int statusCode;
     private String message;
     private Object data;
-
-    public CustomResponse() {}
-
-    public CustomResponse(int statusCode, String message, Object data) {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.data = data;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 
     public static CustomResponse buildErrorResponse(String message,int... statusCode) {
         return new CustomResponse(500, message, null);
